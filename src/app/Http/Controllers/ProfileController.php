827f734c3772002f5c41decc019f94235576f870
ww <ariko_117@hotmail.com>
2024-02-12 15:34:18 +0900

    プロフィール設定修正

--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -16,41 +16,43 @@ class ProfileController extends Controller
         return view('mypage.profile', compact('profile'));
     }
 
-    //　プロフィール登録処理
+    // プロフィール登録処理
     public function postProfile(ProfileRequest $request)
     {
         // ログインしているユーザーのプロフィール情報の取得
-        $profile = auth()->user()->profile;
+        $user = auth()->user();
+        $profile = $user->profile;
         // プロフィールが存在しない場合は新規に作成する
         if (!$profile) {
             $profile = new Profile();
-            $profile->user_id = auth()->user()->id;
+            $profile->user_id = $user->id;
         }
-        // アップロードされたアイコンを保存する処理
-        if ($request->hasFile('icon_url')) {
-            // ディレクトリ名
-            $dir = 'icon';
-            // アップロードされたファイルを取得
-            $file = $request->file('icon_url');
-            // ファイル名を生成
-            $file_extension = $file->getClientOriginalExtension(); // ファイルの拡張子を取得する
-            $file_name = auth()->id() . '_999_' . 'icon' . '.' . $file_extension; // ファイル名に拡張子を連結する
-            // ファイルを保存
-            $file_path = $file->storeAs('public/' . $dir, $file_name);
-            // 一時的にファイルパスを保存
-            $profile->temp_icon_url = $file_path;
-        }
+        // アイコンの画像を保存し、そのURLを取得
+        $iconUrl = $this->storeIconImage($request->file('icon_url'));
         // プロフィール情報を更新する
-        $profile->fill($request->except('icon_url')); // icon_url以外のフォームデータを更新
-        // 最後に保存処理を行う前に、アイコンのURLを正しいフィールドに代入する
-        if (isset($profile->temp_icon_url)) {
-            $profile->icon_url = Storage::url($profile->temp_icon_url);
-            unset($profile->temp_icon_url); // 一時的なフィールドを削除
-        }
+        $profile->fill([
+            'username' => $request->input('username'),
+            'postcode' => $request->input('postcode'),
+            'address' => $request->input('address'),
+            'building' => $request->input('building'),
+            'icon_url' => $iconUrl,
+        ]);
         $profile->save();
-
         return redirect()->route('getMypage')->with([
             'flash_ttl' => '成功', 'flash_msg' => 'プロフィールが更新されました',
         ]);
+    }
+
+    private function storeIconImage($file)
+    {
+        // アップロードされたアイコンを保存する処理
+        if ($file) {
+            $dir = 'icon'; // ディレクトリ名
+            $file_extension = $file->getClientOriginalExtension(); // ファイルの拡張子を取得する
+            $file_name = auth()->id() . '_999_' . 'icon' . '.' . $file_extension; // ファイル名に拡張子を連結する
+            $file_path = $file->storeAs('public/' . $dir, $file_name); // ファイルを保存
+            return Storage::url($file_path); // 保存したファイルのURLを返す
+        }
+        return null; // アップロードされたファイルがない場合はnullを返す
     }
 }
